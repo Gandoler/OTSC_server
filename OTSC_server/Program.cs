437d3510b7fb6a765707428b5005cde58d555237
Gandoler <gl.krutoi@mail.ru>
2025-01-18 15:19:40 +0300

    добавил логирование

--- OTSC_server/Program.cs
+++ OTSC_server/Program.cs
@@ -1,41 +1,58 @@
 using OTSC_server.Telegram;
 using OTSC_server.Telegram.CodeVerification;
 using OTSC_server.Telegram.CodeVerification.CodeFactory;
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+using Serilog;
+internal static class Program
 {
-   
-}
-
-app.MapGet("/VerificationCode/{id:int}", async (int id) =>
-{
-    CodeSenderFactory codeSenderFactory = new("7956821282:AAGfyHzlWYx4hg82r6dwbgTfhH8mX63PCFs");
-    var chechexistchat = codeSenderFactory.CreateCheckExistance();
-    if (await chechexistchat.CheckChatExistance(id))
+    public static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(@"C:\Users\Николай\RiderProjects\OTSC_server\OTSC_server\Logs\myapp.log",
+                rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-        CodeSender sender = codeSenderFactory.CreateCodeSender();
+
+        Log.Information("App start");
+        var builder = WebApplication.CreateBuilder(args);
 
 
-        await sender.SendVerificationCodeAsync(id);
+        var app = builder.Build();
 
-        return Results.Ok(
-            new
+        if (app.Environment.IsDevelopment())
+        {
+
+        }
+
+        app.MapGet("/VerificationCode/{id:int}", async (int id) =>
+        {
+            CodeSenderFactory codeSenderFactory = new("7956821282:AAGfyHzlWYx4hg82r6dwbgTfhH8mX63PCFs");
+            var chechexistchat = codeSenderFactory.CreateCheckExistance();
+            bool userExists = await chechexistchat.CheckChatExistance(id);
+
+            if (userExists)
             {
-                Message = $"Verification Code {id}",
-                Code = sender.Code
-            });
-    }
-    else
-    {
-        return Results.NotFound(new { Message = "User not found" });
-    }
-});
 
-app.Run();
+                CodeSender sender = codeSenderFactory.CreateCodeSender();
+
+
+                await sender.SendVerificationCodeAsync(id);
+                Log.Information($"All Good Code here");
+                return Results.Ok(
+                    new
+                    {
+                        Message = $"Verification Code {id}",
+                        Code = sender.Code
+                    });
+            }
+            else
+            {
+                Log.Information($"user{id} didnt subscribe");
+                return Results.NotFound(new { Message = "User not found" });
+            }
+        });
+
+        app.Run();
+        
+    }
+}
 
